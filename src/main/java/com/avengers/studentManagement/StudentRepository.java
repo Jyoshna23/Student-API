6f51143004877a6ba17fefe15ce8f20ff676bb53
unknown <dammajyoshna123@gmail.com>
2023-01-26 19:24:38 +0530

    Adding First API project

---
+++ src/main/java/com/avengers/studentManagement/StudentRepository.java
@@ -0,0 +1,46 @@
+package com.avengers.studentManagement;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class StudentRepository {
+    Map<Integer,Student> db = new HashMap<>();
+
+
+    public Student getStudent( int admnNo){
+
+        return db.get(admnNo);
+    }
+
+    //Adding the Information of student
+
+    public String addStudent( Student student){
+        int admnNo = student.getAdmnNo();
+        db.put(admnNo,student);
+        return "Student info added successfully";
+    }
+
+
+
+    public String deleteStudent( int id){
+        if(!db.containsKey(id)){
+            return "Invalid id";
+        }
+        db.remove(id);
+        return "Student id deleted Successfully";
+    }
+
+
+    public String updateStudent(int id,int age){
+        if(!db.containsKey(id)){
+            return null;
+        }
+        db.get(id).setAge(age);
+        return "Student info updated successfully";
+    }
+
+}
